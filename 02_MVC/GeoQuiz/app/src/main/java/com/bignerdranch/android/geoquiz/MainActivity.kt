83068e5714c66c484e33435e5cf1a180dd2a28fd
Hady Salhab <hadysalhab1@gmail.com>
2019-10-07 17:28:58 +1100

    Adding Challenge: From Button to ImageButton

--- 02_MVC/GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ 02_MVC/GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -3,6 +3,7 @@ package com.bignerdranch.android.geoquiz
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -11,9 +12,9 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
+    private lateinit var nextButton: ImageButton
     private lateinit var questionTextView: TextView
-    private lateinit var prevButton:Button
+    private lateinit var prevButton:ImageButton
 
     private val questionBank = listOf(
         Question(R.string.question_australia, true),
